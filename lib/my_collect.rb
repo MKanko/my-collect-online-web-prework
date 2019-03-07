206d0305e810565548fbcc92cc779ba7f306b245
Mark Kanko <mkanko66@gmail.com>
2019-03-07 16:30:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
 def my_collect(list)
   modified_list = []
-  item = 0 
-  while item < list.length 
-    modified_list << yield(list[item]) 
-    item = item + 1 
-  end 
+  item = 0
+  while item < list.length
+    modified_list << yield(list[item])
+    item = item + 1
+  end
   modified_list
-end  
-
-
+end
